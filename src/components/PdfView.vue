8c25e577d2ed3082a646ecd41b8c8718cb550372
chenhan <1179991386@qq.com>
2024-05-14 09:52:56 +0800

    feat: pdf文档新增滚动预览功能

--- src/components/PdfView.vue
+++ src/components/PdfView.vue
@@ -68,25 +68,17 @@ const handlePasswordRequest = ({ callback, isWrongPassword }) => {
 <template>
   <div class="pdf-preview">
     <div class="pdf-wrap">
-      <template v-if="isLoading"> Loading... </template>
+      <template v-if="isLoading"> pdf File Loading... </template>
       <vue-pdf-embed
         :source="state.source"
         :style="scale"
         class="vue-pdf-embed"
-        :page="state.pageNum"
         @loaded="handleDocumentLoad"
         @rendered="handleDocumentRender"
         @password-requested="handlePasswordRequest"
         @rendering-failed="handleDocumentRenderFailed"
         @loading-failed="handleDocumentLoadingFailed"
       />
-    </div>
-    <div v-if="!isLoading" class="flex page-tool">
-      <div class="page-tool-item" @click="lastPage">上一页</div>
-      <div class="page-tool-item" @click="nextPage">下一页</div>
-      <div class="page-tool-item">{{ state.pageNum }}/{{ state.numPages }}</div>
-      <div class="page-tool-item" @click="pageZoomOut">放大</div>
-      <div class="page-tool-item" @click="pageZoomIn">缩小</div>
     </div>
   </div>
 </template>
@@ -107,32 +99,10 @@ const handlePasswordRequest = ({ callback, isWrongPassword }) => {
 
 .vue-pdf-embed {
   text-align: center;
-
   height: 100%;
   border: 1px solid #e5e5e5;
   margin: 0 auto;
   box-sizing: border-box;
 }
 
-.page-tool {
-  position: absolute;
-  bottom: 35px;
-  padding-left: 15px;
-  padding-right: 15px;
-  display: flex;
-  align-items: center;
-  background: rgb(66, 66, 66);
-  color: white;
-  border-radius: 19px;
-  z-index: 100;
-  cursor: pointer;
-  margin-left: 50%;
-  transform: translateX(-50%);
-}
-
-.page-tool-item {
-  padding: 8px 15px;
-  padding-left: 10px;
-  cursor: pointer;
-}
 </style>
