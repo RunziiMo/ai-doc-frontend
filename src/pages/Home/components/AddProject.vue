0993a29b8123605530b852bc425b1a34c6e22238
chenhan <1179991386@qq.com>
2024-05-27 01:01:22 +0800

    fix: 文件上传删除

--- src/pages/Home/components/AddProject.vue
+++ src/pages/Home/components/AddProject.vue
@@ -1,61 +1,63 @@
 <template>
-    <el-form v-model="dialogFormVisible" :model="form" label-width="auto" style="max-width: 600px" label-position="top" :rules="rules"
-        ref="ruleFormRef">
-        <el-form-item label="项目空间" prop="itemId">
-            <el-select
-                v-model="form.itemId"
-                filterable
-                remote
-                reserve-keyword
-                placeholder="每个项目必须归属一个项目空间，超级管理员可在后台管理和维护"
-                remote-show-suffix
-                :remote-method="itemSearch"
-                :loading="loading"
-            >
-                <el-option
-                    v-for="item in options"
-                    :key="item"
-                    :label="item.text"
-                    :value="item.id"
-                />
-            </el-select>
-        </el-form-item>
-        <el-form-item label="项目标题" prop="book_name">
-            <el-input v-model="form.book_name" placeholder="项目标题，不超过100字"/>
-        </el-form-item>
-        <el-form-item label="项目标识" prop="identify">
-            <el-input v-model="form.identify" placeholder="项目标识，不超过50字"/>
-        </el-form-item>
-        <el-form-item label="项目描述" prop="description">
-            <el-input type="textarea" v-model="form.description" placeholder="项目描述，不超过500字"/>
-        </el-form-item>
-        <el-form-item label="">
-            <el-radio-group v-model="form.privately_owned">
-                <el-radio :value="1">公开(任何人都可以访问)</el-radio>
-                <!-- <el-radio :value="0">私有(只有参与者或者使用令牌才能访问)</el-radio> -->
-            </el-radio-group>
-        </el-form-item>
-        <el-form-item label="" prop="file">
-            <el-upload  
-                v-model:file-list="form.file"  
-                class="upload-file"  
-                :on-success="handleSuccess"  
-                :auto-upload="false"
-                :before-upload="beforeUpload"
-                multiple  
-                :on-preview="handlePreview"  
-                :on-remove="handleRemove"  
-                :before-remove="beforeRemove"  
-                :limit="1"  
-                :on-exceed="handleExceed"  
-                >  
-                <el-input placeholder="请选择Zip或者Docx文件" style="width: 540px"></el-input>  
-                <el-button type="primary">选择</el-button>  
-            </el-upload>
-        </el-form-item>
+  <el-form
+    v-model="dialogFormVisible"
+    :model="form"
+    label-width="auto"
+    style="max-width: 600px"
+    label-position="top"
+    :rules="rules"
+    ref="ruleFormRef"
+  >
+    <el-form-item label="项目空间" prop="itemId">
+      <el-select
+        v-model="form.itemId"
+        filterable
+        remote
+        reserve-keyword
+        placeholder="每个项目必须归属一个项目空间，超级管理员可在后台管理和维护"
+        remote-show-suffix
+        :remote-method="itemSearch"
+        :loading="loading"
+      >
+        <el-option v-for="item in options" :key="item" :label="item.text" :value="item.id" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="项目标题" prop="book_name">
+      <el-input v-model="form.book_name" placeholder="项目标题，不超过100字" />
+    </el-form-item>
+    <el-form-item label="项目标识" prop="identify">
+      <el-input v-model="form.identify" placeholder="项目标识，不超过50字" />
+    </el-form-item>
+    <el-form-item label="项目描述" prop="description">
+      <el-input type="textarea" v-model="form.description" placeholder="项目描述，不超过500字" />
+    </el-form-item>
+    <el-form-item label="">
+      <el-radio-group v-model="form.privately_owned">
+        <el-radio :value="1">公开(任何人都可以访问)</el-radio>
+        <!-- <el-radio :value="0">私有(只有参与者或者使用令牌才能访问)</el-radio> -->
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item label="" prop="file">
+      <el-upload
+        v-model:file-list="form.file"
+        class="upload-file w-full"
+        :auto-upload="false"
+        :before-upload="beforeUpload"
+        multiple
+        :on-preview="handlePreview"
+        :on-remove="handleRemove"
+        :before-remove="beforeRemove"
+        :limit="1"
+        :on-exceed="handleExceed"
+      >
+        <div class="flex w-full">
+            <el-input placeholder="请选择Zip或者Docx文件" class="flex-1"></el-input>
+            <el-button type="primary">选择</el-button>
+        </div>
+      </el-upload>
+    </el-form-item>
 
-
-        <!-- <el-form-item label="项目功能" prop="selectedAiFunctions">
+    <!-- <el-form-item label="项目功能" prop="selectedAiFunctions">
             <el-checkbox
                 v-model="selectAll"
                 :indeterminate="isIndeterminate"
@@ -73,20 +75,16 @@
             </el-checkbox-group>
         </el-form-item> -->
 
-
-        <el-form-item>
-            <el-button type="primary" @click="submitForm">
-                创建
-            </el-button>
-            <el-button @click="dialogFormVisible = false">取消</el-button>
-        </el-form-item>
-    </el-form>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm"> 创建 </el-button>
+      <el-button @click="dialogFormVisible = false">取消</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 <script lang="ts" setup>
-import { ref, reactive, onMounted, computed } from 'vue';
-import axios from 'axios';
-import { ElMessage } from 'element-plus';
-
+import { ref, reactive, onMounted, computed } from 'vue'
+import axios from 'axios'
+import { ElMessage, ElMessageBox } from 'element-plus'
 
 // checkbox
 const selectAll = ref(false)
@@ -94,180 +92,197 @@ const isIndeterminate = ref(true)
 const selectedAiFunctions = ref(['摘要', '实体提取', '合法审查', '利益分析', '缺漏审查'])
 const aiFunctions = ['摘要', '实体提取', '合法审查', '利益分析', '缺漏审查']
 const handleSelectAllChange = (val: boolean) => {
-    selectedAiFunctions.value = val ? aiFunctions : []
-    isIndeterminate.value = false
+  selectedAiFunctions.value = val ? aiFunctions : []
+  isIndeterminate.value = false
 }
 const handleSelectedAiFunctionsChange = (value: string[]) => {
-    const selectedCount = value.length
-    selectAll.value = selectedCount === aiFunctions.length
-    isIndeterminate.value = selectedCount > 0 && selectedCount < aiFunctions.length
+  const selectedCount = value.length
+  selectAll.value = selectedCount === aiFunctions.length
+  isIndeterminate.value = selectedCount > 0 && selectedCount < aiFunctions.length
 }
 
-
 const props = defineProps({
-    dialogVisible: {
-        type: Boolean,
-        required: true,
-    },
-});
-const emit = defineEmits(['update:dialogVisible']);
+  dialogVisible: {
+    type: Boolean,
+    required: true
+  }
+})
+const emit = defineEmits(['update:dialogVisible'])
 const dialogFormVisible = computed({
-    get() {
-        return props.dialogVisible;  
-    },
-    set(newValue) {
-        emit('update:dialogVisible', newValue);
-    },
-});
+  get() {
+    return props.dialogVisible
+  },
+  set(newValue) {
+    emit('update:dialogVisible', newValue)
+  }
+})
 
-// 表单数据  
+// 表单数据
 const form = reactive({
-    itemId: '',
-    book_name: '',
-    identify: '',
-    description: '',
-    privately_owned: 1, // 假设公开为默认值  
-    file: [] // 文件列表  
-});
+  itemId: '',
+  book_name: '',
+  identify: '',
+  description: '',
+  privately_owned: 1, // 假设公开为默认值
+  file: [] // 文件列表
+})
 
 const rules = {
-    itemId: [
-        { type: 'number', required: true, message: '请输入项目空间', trigger: 'blur' },
-    ],
-    book_name: [
-        { required: true, message: '请输入项目标题', trigger: 'blur' },
-        { min: 1, max: 100, message: '长度在 1 到 100 个字符', trigger: 'blur' }
-    ],
-    identify: [
-        { required: true, message: '请输入项目标识', trigger: 'blur' },
-        { min: 1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur' }
-    ],
-    description: [
-        { required: true, message: '请输入项目描述', trigger: 'blur' },
-        { min: 1, max: 500, message: '长度在 1 到 500 个字符', trigger: 'blur' }
-    ],
-    file: [
-        {
-            required: true,
-            message: '请选择一个文件上传',
-            trigger: 'change',
-        },
-        {
-            validator: (rule, value, callback) => {
-                if (value.length > 0) {
-                    const file = value[0].raw; // 获取第一个文件对象  
-                    const fileType = file.type;
-                    const acceptTypes = ['application/zip', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/pdf'];
-                    if (acceptTypes.indexOf(fileType) === -1) {
-                        return callback(new Error('文件类型必须是 ZIP 或 DOCX 或 PDF'));
-                    }
-                    // 还可以添加其他文件属性检查，比如大小限制等  
-                    callback();
-                } else {
-                    callback(new Error('请选择一个文件上传'));
-                }
-            },
-            trigger: 'change',
-        },
-    ],
-};
+  itemId: [{ type: 'number', required: true, message: '请输入项目空间', trigger: 'blur' }],
+  book_name: [
+    { required: true, message: '请输入项目标题', trigger: 'blur' },
+    { min: 1, max: 100, message: '长度在 1 到 100 个字符', trigger: 'blur' }
+  ],
+  identify: [
+    { required: true, message: '请输入项目标识', trigger: 'blur' },
+    { min: 1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur' }
+  ],
+  description: [
+    { required: true, message: '请输入项目描述', trigger: 'blur' },
+    { min: 1, max: 500, message: '长度在 1 到 500 个字符', trigger: 'blur' }
+  ],
+  file: [
+    {
+      required: true,
+      message: '请选择一个文件上传',
+      trigger: 'change'
+    },
+    {
+      validator: (rule, value, callback) => {
+        if (value.length > 0) {
+          const file = value[0].raw // 获取第一个文件对象
+          const fileType = file.type
+          const acceptTypes = [
+            'application/zip',
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+            'application/pdf'
+          ]
+          if (acceptTypes.indexOf(fileType) === -1) {
+            return callback(new Error('文件类型必须是 ZIP 或 DOCX 或 PDF'))
+          }
+          // 还可以添加其他文件属性检查，比如大小限制等
+          callback()
+        } else {
+          callback(new Error('请选择一个文件上传'))
+        }
+      },
+      trigger: 'change'
+    }
+  ]
+}
 
-// 表单引用，用于表单校验  
-const ruleFormRef = ref(null);
+// 表单引用，用于表单校验
+const ruleFormRef = ref(null)
 
-const beforeUpload = file => {
-    // 处理文件上传的逻辑
+const beforeUpload = (file) => {
+  // 处理文件上传的逻辑
 }
 
 const options = ref([])
 const loading = ref(false)
 const itemSearch = async (query) => {
-    loading.value = true
-    const params = {
-        q: query,
-    }
-    const response = await axios.get("/book/itemsets/search", { params });
-    if (response.data.errcode !== 0) {
-        ElMessage.warning(response.data.message);
-    } else {
-        options.value = response.data.data.results;
-    }
-    loading.value = false;
+  loading.value = true
+  const params = {
+    q: query
+  }
+  const response = await axios.get('/book/itemsets/search', { params })
+  if (response.data.errcode !== 0) {
+    ElMessage.warning(response.data.message)
+  } else {
+    options.value = response.data.data.results
+  }
+  loading.value = false
 }
 
-// 提交表单  
+// 提交表单
 function submitForm() {
-    ruleFormRef.value.validate((valid) => {
-        if (valid) {
-            // 表单校验通过，开始提交表单数据  
-            console.log('提交数据', form);
+  ruleFormRef.value.validate((valid) => {
+    if (valid) {
+      // 表单校验通过，开始提交表单数据
+      console.log('提交数据', form)
 
-            const formData = new FormData();
-            formData.append('itemId', form.itemId);
-            formData.append('book_name', form.book_name);
-            formData.append('identify', form.identify);
-            formData.append('description', form.description);
-            formData.append('privately_owned', form.privately_owned);
-            formData.append('import-file', form.file[0].raw);
-            // formData.append('selectedAiFunctions', selectedAiFunctions.value.join(' '));
-            // console.log("jh_debug selectedAiFunctions:", selectedAiFunctions.value.join(' '));
-            
-            axios.post('/book/users/import', formData) // 替换为实际的API地址  
-                .then(response => {
-                    if (response.data.errcode == 0) {
-                        ElMessage.success('项目创建成功');
-                        // 重置表单
-                        form.itemId = 0;
-                        form.book_name = '';
-                        form.identify = '';
-                        form.description = '';
-                        form.privately_owned = 1;
-                        form.file = [];
-                        dialogFormVisible.value = false;
-                    } else {
-                        ElMessage.error('项目创建失败: ' + response.data.message);
-                    }
-                })
-                .catch(error => {
-                    console.error('请求失败:', error);
-                    ElMessage.error('请求失败，请稍后再试');
-                });
-        } else {
-            // 表单校验不通过，提示用户错误信息  
-            return false;
-        }
-    });
+      const formData = new FormData()
+      formData.append('itemId', form.itemId)
+      formData.append('book_name', form.book_name)
+      formData.append('identify', form.identify)
+      formData.append('description', form.description)
+      formData.append('privately_owned', form.privately_owned)
+      formData.append('import-file', form.file[0].raw)
+      // formData.append('selectedAiFunctions', selectedAiFunctions.value.join(' '));
+      // console.log("jh_debug selectedAiFunctions:", selectedAiFunctions.value.join(' '));
+
+      axios
+        .post('/book/users/import', formData) // 替换为实际的API地址
+        .then((response) => {
+          if (response.data.errcode == 0) {
+            ElMessage.success('项目创建成功')
+            // 重置表单
+            form.itemId = 0
+            form.book_name = ''
+            form.identify = ''
+            form.description = ''
+            form.privately_owned = 1
+            form.file = []
+            dialogFormVisible.value = false
+          } else {
+            ElMessage.error('项目创建失败: ' + response.data.message)
+          }
+        })
+        .catch((error) => {
+          console.error('请求失败:', error)
+          ElMessage.error('请求失败，请稍后再试')
+        })
+    } else {
+      // 表单校验不通过，提示用户错误信息
+      return false
+    }
+  })
 }
 
-// 假设的文件上传前的处理逻辑  
-function beforeRemove(file, fileList) {
-    return this.$confirm(`确定移除 ${file.name}？`);
+// 假设的文件上传前的处理逻辑
+async function beforeRemove(file, fileList) {
+  return new Promise((resolve, reject) => {
+    ElMessageBox.confirm(`确定移除 ${file.name}？`)
+      .then(() => {
+        resolve(true)
+      })
+      .catch(() => {
+        resolve(false)
+      })
+  })
 }
 
-// 处理文件超出限制的情况  
+// 处理文件超出限制的情况
 function handleExceed(files, fileList) {
-    this.$message.warning(`当前限制选择 1 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+  this.$message.warning(
+    `当前限制选择 1 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`
+  )
 }
 
-// 处理文件预览  
+// 处理文件预览
 function handlePreview(file) {
-    console.log(file);
+  console.log(file)
 }
 
-// 处理文件移除  
+// 处理文件移除
 function handleRemove(file, fileList) {
-    console.log(file, fileList);
+  console.log(file, fileList)
 }
 
-// 初始化数据，如果需要从后端获取初始数据，可以在这里使用axios请求  
+// 初始化数据，如果需要从后端获取初始数据，可以在这里使用axios请求
 onMounted(async () => {
-    // 假设我们从后端获取了一些初始数据，这里用模拟数据代替  
-    const mockData = {
-        // 这里是假设从后端获取的初始数据格式  
-        // 根据实际情况调整  
-    };
+  // 假设我们从后端获取了一些初始数据，这里用模拟数据代替
+  const mockData = {
+    // 这里是假设从后端获取的初始数据格式
+    // 根据实际情况调整
+  }
 
-    // 将获取的数据设置到表单中  
-    // Object.assign(form, mockData);  
-});  
+  // 将获取的数据设置到表单中
+  // Object.assign(form, mockData);
+})
 </script>
+<style scoped>
+:deep(.el-upload) {
+    width: 100% !important;
+}
+</style>
