679f44b0aecd9f7064e08bc3041d697ced9aec8c
chenhan <1179991386@qq.com>
2024-09-11 23:53:34 +0800

    fix: 解决pdf导出显示不全的问题

--- src/pages/BookReader/components/ExportDialog.vue
+++ src/pages/BookReader/components/ExportDialog.vue
@@ -1,208 +1,196 @@
 <template>
-    <el-dialog v-model="dialogFormVisible" title="对话导出">
-        <div ref="contentToCopy" style="height: 500px; overflow: auto;">
-            <ExportMessage
-                v-for="m in messages"
-                :message="m"
-                :document="document"
-            />
-        </div>
-        <template #footer>
-            <div class="dialog-footer">
-                <el-button type="primary" @click="dialogFormVisible = false">
-                    关闭
-                </el-button>
-                <el-button type="primary" @click="copyToClipboard">
-                    复制
-                </el-button>
-                <el-button type="primary" @click="handleExportWord">
-                    word导出
-                </el-button>
-                <el-button type="primary" @click="handleExportPdf">
-                    pdf导出
-                </el-button>
-                <el-button type="primary" @click="handleExportTxt">
-                    txt导出
-                </el-button>
-            </div>
-        </template>
-    </el-dialog>
+  <el-dialog v-model="dialogFormVisible" title="对话导出">
+    <div ref="contentToCopy" style="height: 500px; overflow: auto">
+      <div ref="messagesRef">
+        <ExportMessage v-for="m in messages" :message="m" :document="document" />
+      </div>
+    </div>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button type="primary" @click="dialogFormVisible = false"> 关闭 </el-button>
+        <el-button type="primary" @click="copyToClipboard"> 复制 </el-button>
+        <el-button type="primary" @click="handleExportWord"> word导出 </el-button>
+        <el-button type="primary" @click="handleExportPdf"> pdf导出 </el-button>
+        <el-button type="primary" @click="handleExportTxt"> txt导出 </el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
 
 <script lang="ts" setup>
 import { reactive, ref, computed, inject } from 'vue'
 import { marked } from 'marked'
 import hljs from 'highlight.js'
-import jsPDF from 'jspdf';
+import jsPDF from 'jspdf'
 import 'highlight.js/styles/github-dark.css'
 import { ElMessage } from 'element-plus'
 import { exportToTxt, exportWord } from '@/utils/file'
 import ExportMessage from './ExportMessage.vue'
+import html2canvas from 'html2canvas';
 
-const props = defineProps({  
-    showDialog: {
-        type: Boolean, 
-        required: true,
-    },
-    document: {
-        type: Object,
-        required: true,
-    },
-    messages: {
-        type: Array,
-        required: true,
-    }
-});
+const props = defineProps({
+  showDialog: {
+    type: Boolean,
+    required: true
+  },
+  document: {
+    type: Object,
+    required: true
+  },
+  messages: {
+    type: Array,
+    required: true
+  }
+})
 
-const renderer = new marked.Renderer();
+const renderer = new marked.Renderer()
 renderer.code = (code, language) => {
-    const validLang = hljs.getLanguage(language) ? language : 'plaintext';
-    const highlightedCode = hljs.highlight(code, { language: validLang }).value;
-    return `<div class="code-header hljs ${validLang}">
+  const validLang = hljs.getLanguage(language) ? language : 'plaintext'
+  const highlightedCode = hljs.highlight(code, { language: validLang }).value
+  return `<div class="code-header hljs ${validLang}">
         </div>
         <pre class="hljs ${validLang}"><code class="code-body">${highlightedCode}</code>
-    </pre>`;
-};
+    </pre>`
+}
 renderer.table = (header, body) => {
-    return `<table class="el-table chat-response-table">
+  return `<table class="el-table chat-response-table">
         <thead>${header}</thead>
         <tbody>${body}</tbody>
-    </table>`;
-};
-marked.setOptions({ renderer });
+    </table>`
+}
+marked.setOptions({ renderer })
 
-const checkedMessages = inject('checkedMessages');
-const checkMessage = inject('checkMessage');
-const messages = computed(() => props.messages.filter(item => checkedMessages.value.includes(item.message_id)));
+const checkedMessages = inject('checkedMessages')
+const checkMessage = inject('checkMessage')
+const messages = computed(() =>
+  props.messages.filter((item) => checkedMessages.value.includes(item.message_id))
+)
 const renderedHtmls = computed(() => {
-    const messages = props.messages.filter(item => checkedMessages.value.includes(item.message_id))
-    const renderedHtmls = messages.map(item => {
-        console.log(item.response);
-        const regex = /```json\n([\s\S]+?)\n```/g;
-        let match;
-        while ((match = regex.exec(item.response)) !== null) {
-            const code = match[1];
-            return marked(`\`\`\`json\n${code}\n\`\`\``);
-        }
-        // 没有json内容，直接渲染原文
-        return marked(item.response)
-    });
-    console.log(renderedHtmls.length);
-    return renderedHtmls;
-});
+  const messages = props.messages.filter((item) => checkedMessages.value.includes(item.message_id))
+  const renderedHtmls = messages.map((item) => {
+    console.log(item.response)
+    const regex = /```json\n([\s\S]+?)\n```/g
+    let match
+    while ((match = regex.exec(item.response)) !== null) {
+      const code = match[1]
+      return marked(`\`\`\`json\n${code}\n\`\`\``)
+    }
+    // 没有json内容，直接渲染原文
+    return marked(item.response)
+  })
+  console.log(renderedHtmls.length)
+  return renderedHtmls
+})
 
-const contentToCopy = ref();
+const contentToCopy = ref()
+const messagesRef = ref()
 
 const convertToJSON = (message) => {
-    const sections = message.split(/\d+\.\s\*\*(.+?)\*\*：/).filter(Boolean);
-    console.log(sections);
-    const json = {};
+  const sections = message.split(/\d+\.\s\*\*(.+?)\*\*：/).filter(Boolean)
+  console.log(sections)
+  const json = {}
 
-    for (let i = 0; i < sections.length; i += 2) {
-        console.log(sections[i]);
-        const sectionTitle = sections[i].trim();
-        const sectionContent = sections[i + 1].trim();
-        json[sectionTitle] = parseContent(sectionContent);
-    }
-    return JSON.stringify(json, null, 2);
+  for (let i = 0; i < sections.length; i += 2) {
+    console.log(sections[i])
+    const sectionTitle = sections[i].trim()
+    const sectionContent = sections[i + 1].trim()
+    json[sectionTitle] = parseContent(sectionContent)
+  }
+  return JSON.stringify(json, null, 2)
 }
 
 const parseContent = (content) => {
-    const lines = content.split('\n').filter(line => line.trim());
-    const result = {};
+  const lines = content.split('\n').filter((line) => line.trim())
+  const result = {}
 
-    lines.forEach(line => {
-        const keyValueMatch = line.match(/- (.+?)：(.+)/);
-        if (keyValueMatch) {
-            result[keyValueMatch[1].trim()] = keyValueMatch[2].trim();
-        } else {
-            // If the line does not match key-value pair format, we might want to process it differently
-            // Here, we are just adding it as a text to a "details" key, you can adjust this logic based on your need
-            if (!result.details) {
-                result.details = [];
-            }
-            result.details.push(line.trim());
-        }
-    });
-
-    // If 'details' exists and has only one item, unnest it for simplicity
-    if (result.details && result.details.length === 1) {
-        result.details = result.details[0];
+  lines.forEach((line) => {
+    const keyValueMatch = line.match(/- (.+?)：(.+)/)
+    if (keyValueMatch) {
+      result[keyValueMatch[1].trim()] = keyValueMatch[2].trim()
+    } else {
+      // If the line does not match key-value pair format, we might want to process it differently
+      // Here, we are just adding it as a text to a "details" key, you can adjust this logic based on your need
+      if (!result.details) {
+        result.details = []
+      }
+      result.details.push(line.trim())
     }
+  })
 
-    return result;
+  // If 'details' exists and has only one item, unnest it for simplicity
+  if (result.details && result.details.length === 1) {
+    result.details = result.details[0]
+  }
+
+  return result
 }
 
 const copyToClipboard = () => {
-    const content = contentToCopy.value;
-    if (content) {
-        // Create a range and select the content
-        const range = document.createRange();
-        range.selectNodeContents(content);
+  const content = contentToCopy.value
+  if (content) {
+    // Create a range and select the content
+    const range = document.createRange()
+    range.selectNodeContents(content)
 
-        // Get the selection object and remove any existing selections
-        const selection = window.getSelection();
-        selection.removeAllRanges();
-        selection.addRange(range);
+    // Get the selection object and remove any existing selections
+    const selection = window.getSelection()
+    selection.removeAllRanges()
+    selection.addRange(range)
 
-        // Copy the selected content to the clipboard
-        try {
-            document.execCommand('copy');
-            ElMessage.success('已复制到剪贴板！');
-        } catch (err) {
-            console.error('Failed to copy text: ', err);
-        }
-        // Remove the selection
-        selection.removeAllRanges();
+    // Copy the selected content to the clipboard
+    try {
+      document.execCommand('copy')
+      ElMessage.success('已复制到剪贴板！')
+    } catch (err) {
+      console.error('Failed to copy text: ', err)
     }
-    dialogFormVisible.value = false;
+    // Remove the selection
+    selection.removeAllRanges()
+  }
+  dialogFormVisible.value = false
 }
 
-const emit = defineEmits(['update:showDialog']);
+const emit = defineEmits(['update:showDialog'])
 
 const dialogFormVisible = computed({
-    get() {
-        return props.showDialog;  
-    },
-    set(newValue) {
-        emit('update:showDialog', newValue);
-    },
-});
+  get() {
+    return props.showDialog
+  },
+  set(newValue) {
+    emit('update:showDialog', newValue)
+  }
+})
 
 const handleExportWord = () => {
-    exportWord(contentToCopy.value, props.document.doc_name)
+  exportWord(contentToCopy.value, props.document.doc_name)
 }
 
 const handleExportPdf = () => {
-    // 创建一个新的PDF文档
-    const pdf = new jsPDF();
-    // 添加中文字体支持（需要额外的字体文件，例如Base4）
-    pdf.addFont('/font/simhei.ttf', 'simheii', 'normal');
-    // 使用中文字体
-    pdf.setFont('simheii');
-    // 添加文本到PDF文档
-    pdf.text(contentToCopy.value.textContent, 5, 5);
-    // 保存PDF（默认下载）
-    pdf.save(`${props.document.doc_name}.pdf`);
+    html2canvas(messagesRef.value).then(canvas => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF();
+        const width = pdf.internal.pageSize.getWidth();
+        const height = pdf.internal.pageSize.getHeight(); // 减去页眉和页脚的高度
+        pdf.addImage(imgData, 'PNG', 4, 4, width, height);
+        pdf.save(`${props.document.doc_name}.pdf`)
+    });
 }
 
 const handleExportTxt = () => {
-    exportToTxt(contentToCopy.value.textContent, `${props.document.doc_name}.txt`)
+  exportToTxt(contentToCopy.value.textContent, `${props.document.doc_name}.txt`)
 }
 </script>
 
 <style>
-
-
 .hljs {
-    overflow-x: scroll;
+  overflow-x: scroll;
 }
 .chat-response-table {
-    width: 100%;
-    overflow-x: scroll;
+  width: 100%;
+  overflow-x: scroll;
 }
 .code-header {
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
